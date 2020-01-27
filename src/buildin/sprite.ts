487cf9a1810d01b44dd5eb0edb6aaef8cf7606df
afterwind-io <spmushroom@hotmail.com>
2020-01-27 13:54:51 +0800

    :sparkles: (Sprite) Add the ability to display partial source image;

--- src/buildin/sprite.ts
+++ src/buildin/sprite.ts
@@ -5,6 +5,8 @@ export class Sprite extends Entity {
   public readonly customDrawing: boolean = true;
   public name: string = "Sprite";
 
+  public clipOrigin: Vector2 = new Vector2();
+  public clipSize: Vector2 = new Vector2();
   public offset: Vector2 = new Vector2();
 
   protected image: HTMLImageElement = new Image();
@@ -23,11 +25,49 @@ export class Sprite extends Entity {
       return;
     }
 
+    const sx = this.clipOrigin.x;
+    const sy = this.clipOrigin.y;
+    const sw = this.clipSize.x || this.image.naturalWidth;
+    const sh = this.clipSize.y || this.image.naturalHeight;
+
     const dx = this.isCentered ? -this.w / 2 : this.offset.x;
     const dy = this.isCentered ? -this.h / 2 : this.offset.y;
+    const dw = this.w || sw;
+    const dh = this.h || sh;
 
     ctx.imageSmoothingEnabled = this.isSmoothImage;
-    ctx.drawImage(this.image, dx, dy, this.w, this.h);
+    ctx.drawImage(this.image, sx, sy, sw, sh, dx, dy, dw, dh);
+  }
+
+  /**
+   * Set the actual display region from the source image.
+   * 
+   * You can **clip** a sub-rectangle from the source image, by specifying
+   * a top-left starting point, and the width and height of the region.
+   * 
+   * If the `sw` is not specified, the entire width from the `sx` to the right
+   * will be applied. Also, if the `sh` is not specified, the entire height from
+   * the `sy` to the bottom will be applied.
+   *
+   * @param {number} sx
+   * The x-axis coordinate of the top left corner of the sub-rectangle of the source image;
+   * @param {number} sy
+   * The y-axis coordinate of the top left corner of the sub-rectangle of the source image;
+   * @param {number} [sw=0]
+   * The width of the sub-rectangle of the source image;
+   * @param {number} [sh=0]
+   * The height of the sub-rectangle of the source image;
+   * 
+   * @returns {this}
+   * @memberof Sprite
+   */
+  public SetClip(sx: number, sy: number, sw: number = 0, sh: number = 0): this {
+    this.clipOrigin.x = sx;
+    this.clipOrigin.y = sy;
+    this.clipSize.x = sw;
+    this.clipSize.y = sh;
+
+    return this;
   }
 
   /**
