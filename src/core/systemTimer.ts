a4ae8e9cc907bcc5d30bde6a621ac26b2c64efd7
afterwind-io <spmushroom@hotmail.com>
2021-04-30 16:19:34 +0800

    :bug: (Core) Fix delta error of the first frame

--- src/core/systemTimer.ts
+++ src/core/systemTimer.ts
@@ -1,17 +1,30 @@
 export class SystemTimer {
-  private lastUpdate: number = performance.now();
+  public Tick: () => number;
+
+  private lastUpdate: number = 0;
   private lastDelta: number = 0;
+
+  public constructor() {
+    this.Tick = this.coldTick;
+  }
 
   public get Delta(): number {
     return this.lastDelta;
   }
 
-  public Tick(): number {
+  private coldTick = () => {
+    this.Tick = this.hotTick;
+
+    this.lastUpdate = performance.now() / 1000;
+    return 1 / 60;
+  };
+
+  private hotTick = () => {
     const timestamp = performance.now() / 1000;
 
     this.lastDelta = timestamp - this.lastUpdate;
     this.lastUpdate = timestamp;
 
     return this.lastDelta;
-  }
+  };
 }
