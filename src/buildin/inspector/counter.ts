4055d7e251fd424de54bc0c9425be86ba8511375
afterwind-io <spmushroom@hotmail.com>
2021-05-18 18:13:34 +0800

    :sparkles: (Inspector) Add widget counter

--- src/buildin/inspector/counter.ts
+++ src/buildin/inspector/counter.ts
@@ -4,44 +4,66 @@ import { Edge } from '../ui/common/edge';
 import { Text } from '../ui/text';
 import { Timer } from '../timer';
 import { Engine } from '../../core/engine';
-import { Flex } from '../ui/flex';
+import { Flex, FlexDirection } from '../ui/flex';
 import { Checkbox } from '../ui/checkbox';
+import { Widget } from '../ui/foundation/widget';
 
 export class InspectorCounter extends SingleChildWidget {
   public readonly name: string = 'InspectorCounter';
 
   protected readonly isLooseBox: boolean = false;
 
-  private $checkbox!: Checkbox;
-  private $label!: Text;
   private $timer: Timer = new Timer(1, true);
 
-  private _enabled: boolean = true;
+  private $checkbox!: Checkbox;
+  private $entityLabel!: Text;
+  private $widgetLabel!: Text;
+
+  private _enable: boolean = true;
 
   public _Ready() {
     this.AddChild(
       new Container({
         margin: Edge.Bottom(4),
         child: new Flex({
+          direction: FlexDirection.Vertical,
           children: [
+            new Flex({
+              children: [
+                new Container({
+                  margin: new Edge(0, 4, 0, 4),
+                  child: (this.$checkbox = new Checkbox({
+                    width: 12,
+                    height: 12,
+                    checked: this._enable,
+                  })),
+                }),
+                new Text({
+                  content: 'Counter',
+                }),
+              ],
+            }),
+
             new Container({
-              margin: Edge.Right(4),
-              child: (this.$checkbox = new Checkbox({
-                width: 12,
-                height: 12,
-                checked: this._enabled,
+              margin: new Edge(16, 4, 0, 4),
+              child: (this.$entityLabel = new Text({
+                content: 'Entity Count',
               })),
             }),
-            (this.$label = new Text({
-              content: 'Entity Count',
-            })),
+
+            new Container({
+              margin: new Edge(16, 4, 0, 4),
+              child: (this.$widgetLabel = new Text({
+                content: 'Widget Count',
+              })),
+            }),
           ],
         }),
       })
     );
 
     this.$checkbox.Connect('OnToggle', (checked) => {
-      this._enabled = checked;
+      this._enable = checked;
     });
 
     this.$timer = new Timer(1, true);
@@ -50,14 +72,19 @@ export class InspectorCounter extends SingleChildWidget {
   }
 
   private Refresh() {
-    let content = '-';
+    let entity = 0;
+    let widget = 0;
 
-    if (this._enabled) {
-      let i = 0;
-      Engine.Current.nodeRoot!.Traverse((_) => (i++, void 0));
-      content = `${i}`;
+    if (this._enable) {
+      Engine.Current.nodeRoot!.Traverse((node) => {
+        entity++;
+        if (node instanceof Widget) {
+          widget++;
+        }
+      });
     }
 
-    this.$label.SetContent(`Entity Count: ${content}`);
+    this.$entityLabel.SetContent(`Entity Count: ${entity || '-'}`);
+    this.$widgetLabel.SetContent(`Widget Count: ${widget || '-'}`);
   }
 }
