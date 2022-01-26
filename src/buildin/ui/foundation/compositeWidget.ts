39c8acbb52c179d0c47d2d8167b258e8b62239a3
afterwind-io <spmushroom@hotmail.com>
2022-01-26 11:20:46 +0800

    :hammer: (ui) CompositeWidget should follow its child size

--- src/buildin/ui/foundation/compositeWidget.ts
+++ src/buildin/ui/foundation/compositeWidget.ts
@@ -7,8 +7,9 @@ import { Widget } from "./widget";
  * 组合容器组件抽象类。
  *
  * 该抽象代表了一类组件，它们：
- * - 不具备期望尺寸，其本身只是一系列子组件的组合容器；
+ * - 只是一系列子组件的组合容器；
  * - 可能会输出不同的子组件组合，但其在任何时刻最多只拥有一个子组件节点；
+ * - 不具备期望尺寸，其尺寸等同于其唯一子组件的大小；
  * - 可能拥有自己的状态；
  *
  * 绝大多数的业务组件都应该继承此类。
@@ -20,9 +21,14 @@ export abstract class CompositeWidget<
   protected _Layout(constraint: Constraint): Size {
     const child = this.GetFirstChild();
     if (child) {
-      return child.$Layout(constraint);
+      const size = child.$Layout(constraint);
+      this._intrinsicWidth = size.width;
+      this._intrinsicHeight = size.height;
+      return size;
     }
 
+    this._intrinsicHeight = 0;
+    this._intrinsicWidth = 0;
     return { width: 0, height: 0 };
   }
 
