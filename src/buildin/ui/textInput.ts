1ea4e1fd4449cc2d2d1e714c0b61b83d0513a91c
afterwind-io <spmushroom@hotmail.com>
2022-02-03 11:01:06 +0800

    :hammer: (ui) Refine the mocking of HtmlInputElement

--- src/buildin/ui/textInput.ts
+++ src/buildin/ui/textInput.ts
@@ -1,9 +1,13 @@
 import { CanvasManager } from "../../core/manager/canvas";
+import { Align, Alignment } from "./align";
 import { Length } from "./common/types";
-import { BindThis } from "./foundation/decorator";
+import { SwitchCursor } from "./common/utils";
+import { BindThis, Reactive } from "./foundation/decorator";
 import { SingleChildWidget } from "./foundation/singleChildWidget";
 import { SizableWidgetOptions, WidgetElement } from "./foundation/types";
 import { MouseSensor } from "./mouseSensor";
+import { Opacity } from "./opacity";
+import { Text } from "./text";
 
 interface TextInputOptions extends SizableWidgetOptions {
   value: string;
@@ -23,11 +27,11 @@ interface TextInputOptions extends SizableWidgetOptions {
  */
 export class TextInput extends SingleChildWidget<TextInputOptions> {
   public readonly name: string = "TextInput";
-  public readonly enableDrawing: boolean = true;
 
   protected readonly isLooseBox: boolean = false;
 
-  private _inputElement!: HTMLInputElement;
+  private $inputElement!: HTMLInputElement;
+  private _isFocused: boolean = false;
 
   protected _Ready(): void {
     const input = document.createElement("input");
@@ -36,54 +40,36 @@ export class TextInput extends SingleChildWidget<TextInputOptions> {
     input.style.border = "none";
     input.style.outline = "none";
     input.style.background = "none";
-
-    // 初始挂载暂时隐藏，避免元素闪动
     input.style.display = "none";
 
     input.onchange = this.OnInputChange;
+    input.onblur = this.OnInputBlur;
 
     CanvasManager.container.appendChild(input);
-    this._inputElement = input;
+    this.$inputElement = input;
   }
 
   protected _Destroy(): void {
-    this._inputElement.onchange = null;
+    this.$inputElement.onchange = null;
 
-    CanvasManager.container.removeChild(this._inputElement);
+    CanvasManager.container.removeChild(this.$inputElement);
 
     // @ts-expect-error TS2322 clear reference
-    this._inputElement = null;
-  }
-
-  public _Draw(ctx: CanvasRenderingContext2D): void {
-    const inputElement = this._inputElement;
-
-    // FIXME 是否显示input理论上需要检查:
-    // - 当前元素是否显示在canvas上
-    // - 当前元素是否被composition裁剪
-    inputElement.style.display = this.globalVisible ? "block" : "none";
-
-    const width = this._intrinsicWidth;
-    const height = this._intrinsicHeight;
-    inputElement.style.width = `${width}px`;
-    inputElement.style.height = `${height}px`;
-
-    // FIXME 只有Draw阶段才能拿到Input的真实位置，理论上应该在某个特定生命周期钩子处理
-    const { x, y } = this.ConvertToScreenPosition(this.position);
-    inputElement.style.left = `${x}px`;
-    inputElement.style.top = `${y}px`;
+    this.$inputElement = null;
   }
 
   protected GetHeight(): Length {
-    return this.options.height || "stretch";
+    return this.options.height!;
   }
 
   protected GetWidth(): Length {
-    return this.options.width || "stretch";
+    return this.options.width!;
   }
 
   protected NormalizeOptions(options: TextInputOptions): TextInputOptions {
     return {
+      width: "stretch",
+      height: "stretch",
       color: "black",
       fontName: "",
       fontSize: 12,
@@ -93,37 +79,85 @@ export class TextInput extends SingleChildWidget<TextInputOptions> {
     };
   }
 
+  @Reactive
+  private OnFocus(): void {
+    this.LayoutInputElement();
+
+    this.$inputElement.style.display = "block";
+    this.$inputElement.focus();
+
+    this._isFocused = true;
+  }
+
+  @BindThis
+  private OnHover(isHovering: boolean): void {
+    SwitchCursor(isHovering && !this._isFocused, "text");
+  }
+
   @BindThis
   private OnInputChange(): void {
-    const inputElement = this._inputElement;
-
     // 屏蔽因为blur触发的change事件
-    if (document.activeElement !== inputElement) {
+    if (document.activeElement !== this.$inputElement) {
       return;
     }
 
-    this.options.onChange(inputElement.value);
+    this.options.onChange(this.$inputElement.value);
+  }
+
+  @Reactive
+  private OnInputBlur(): void {
+    this.$inputElement.style.display = "none";
+
+    this._isFocused = false;
+  }
+
+  private LayoutInputElement(): void {
+    const inputElement = this.$inputElement;
+
+    inputElement.style.width = `${this._intrinsicWidth}px`;
+    inputElement.style.height = `${this._intrinsicHeight}px`;
+
+    // FIXME 只有Layout阶段之后才能拿到Input的真实位置，理论上应该在某个特定生命周期钩子处理
+    const { x, y } = this.ConvertToScreenPosition(this.position);
+    inputElement.style.left = `${x}px`;
+    inputElement.style.top = `${y}px`;
   }
 
   protected _Render(): WidgetElement {
     const { value, placeholder, color, fontName, fontSize, fontWeight } = this
       .options as Required<TextInputOptions>;
 
-    const inputElement = this._inputElement;
+    const inputElement = this.$inputElement;
     inputElement.value = value;
-    inputElement.placeholder = placeholder;
     inputElement.style.color = color;
     inputElement.style.fontFamily = fontName;
     inputElement.style.fontSize = `${fontSize}px`;
     inputElement.style.fontWeight = `${fontWeight}`;
 
     const { width, height } = this.options as Required<TextInputOptions>;
+    const placeholderWidget = !this._isFocused
+      ? new Align({
+          alignment: Alignment.Left,
+          width,
+          height,
+          child: new Opacity({
+            opacity: 0.5,
+            child: new Text({
+              content: placeholder,
+              fillStyle: color,
+              fontName: fontName,
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+            }),
+          }),
+        })
+      : null;
     return new MouseSensor({
       width,
       height,
-      onClick: () => {
-        this._inputElement.focus();
-      },
+      onClick: this.OnFocus,
+      onHover: this.OnHover,
+      child: placeholderWidget,
     });
   }
 }
